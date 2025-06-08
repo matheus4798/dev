0dd4b9bad9af2980d43f99812ccee5367b868abf
matheus4798 <146399118+matheus4798@users.noreply.github.com>
2025-06-08 19:47:32 +0000

    1

---
+++ src/services/credenciaisFirebase.js
@@ -0,0 +1,21 @@
+// src/services/credenciaisFirebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "........................................",
+  authDomain: "....................",
+  projectId: "....................",
+  storageBucket: "....................",
+  messagingSenderId: "....................",
+  appId: "....................",
+};
+
+// Inicializa o App
+const appFirebase = initializeApp(firebaseConfig);
+
+// **NOVO**: inicializa e exporta o Firestore
+export const db = getFirestore(appFirebase);
+
+// Mantém export default do App (útil caso queira)
+export default appFirebase;
